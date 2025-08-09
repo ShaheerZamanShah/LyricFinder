03831bc1d667f155051d83c7bc880183cc7bae72
ShaheerZamanShah <shaheer@taskflow.com>
2025-08-10 01:29:27 +0500

    Fix React error #130: Remove DarkVeil WebGL component causing production issues

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,21 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Header from './components/Header';
-import DarkVeil from './components/DarkVeil';
 import SpotifyCallback from './components/SpotifyCallback';
 import { ThemeProvider } from './contexts/ThemeContext';
+
+// Simple background component without WebGL
+const SimpleBackground = () => {
+  return (
+    <div className="fixed inset-0 -z-10 bg-gradient-to-br from-purple-900/20 via-blue-900/20 to-green-900/20" />
+  );
+};
 
 const AppContent = () => {
   return (
     <Router>
       <div className="min-h-screen flex flex-col relative transition-colors duration-300">
-        <DarkVeil />
+        <SimpleBackground />
         <Routes>
           <Route path="/" element={
             <>
