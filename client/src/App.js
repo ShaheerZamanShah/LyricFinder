6c3a5ef4676edfd7e9aea58099b14ff3dacf019b
ShaheerZamanShah <shaheer@taskflow.com>
2025-08-10 00:05:26 +0500

    Restore full LyricFinder app with Railway backend URL

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
-import TestHome from './pages/TestHome';
+import Home from './pages/Home';
 import SpotifyCallback from './components/SpotifyCallback';
 import DarkVeil from './components/DarkVeil';
 import { ThemeProvider, useTheme } from './contexts/ThemeContext';
@@ -65,7 +65,7 @@ const AppContent = () => {
             <>
               <Header />
               <main className="flex-1 p-5 max-w-6xl mx-auto w-full">
-                <TestHome />
+                <Home />
               </main>
             </>
           } />
