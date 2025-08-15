6c9fc8271d129a096641ec1b47077c4a3f68d99d
ShaheerZamanShah <shaheer@taskflow.com>
2025-08-15 21:34:09 +0500

    Restore DarkVeil background for the entire website.

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Stats from './pages/Stats';
 import Header from './components/Header';
+import DarkVeil from './components/DarkVeil';
 import './App.css';
 import { ThemeProvider } from './contexts/ThemeContext';
 
@@ -10,18 +11,17 @@ function App() {
   return (
     <ThemeProvider>
       <Router>
-        <div className="App">
-          <Header />
-          <main>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/stats" element={
-                <div className="min-h-screen bg-gradient-to-br from-gray-900 via-blue-900 to-purple-900">
-                  <Stats />
-                </div>
-              } />
-            </Routes>
-          </main>
+        <div className="App" style={{ position: 'relative', minHeight: '100vh', overflow: 'hidden' }}>
+          <DarkVeil style={{ position: 'fixed', inset: 0, zIndex: 0 }} />
+          <div style={{ position: 'relative', zIndex: 1 }}>
+            <Header />
+            <main>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/stats" element={<Stats />} />
+              </Routes>
+            </main>
+          </div>
         </div>
       </Router>
     </ThemeProvider>
